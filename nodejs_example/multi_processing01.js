401ac5f66009f0702f7c39e6ee3f02e2d4c9808a
charica12 <shdkwnffl@gmail.com>
2021-04-29 10:18:43 +0900

    20210429_1018

---
+++ nodejs_example/multi_processing01.js
@@ -0,0 +1,9 @@
+const Pool = require('multiprocessing').Pool;
+
+function square(x) {
+  return x * x;
+}
+
+const pool = new Pool(4);  // spawns 4 child processes to complete your jobs
+
+pool.map([1, 2, 3], square).then(result => console.log(result));
